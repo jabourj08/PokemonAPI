6a982f31d4536b6047de9e257d16472ce20a50cf
Josh <jabourjosh@gmail.com>
2020-09-02 17:09:04 -0400

    Re-commit for the first time third time

---
+++ PokemonAPI/Controllers/PokemonController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PokemonAPI.Models;
+
+namespace PokemonAPI.Controllers
+{
+    public class PokemonController : Controller
+    {
+        private readonly PokemonDAL _pokemonDAL;
+
+        public PokemonController()
+        {
+            _pokemonDAL = new PokemonDAL();
+        }
+
+        public async Task<IActionResult> PokemonJSON()
+        {
+            var pokemonJSON = await _pokemonDAL.GetRawJSON();
+
+            return View(pokemonJSON);
+        }
+
+        public async Task<IActionResult> GetPokemon()
+        {
+            var pokemon = await _pokemonDAL.GetPokemon();
+
+            return View(pokemon);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PokemonAPI/Models/PokemonDAL.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PokemonAPI.Models
+{
+    public class PokemonDAL
+    {
+        public HttpClient GetClient()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://pokeapi.co/api/v2/");
+
+            return client;
+        }
+
+        public async Task<string> GetRawJSON()
+        {
+            var client = GetClient();
+            var response = await client.GetAsync("pokemon/3");
+            var pokemonJSON = await response.Content.ReadAsStringAsync();
+
+            return pokemonJSON;
+        }
+
+        public async Task<Pokemon> GetPokemon()
+        {
+            var client = GetClient();
+            var response = await client.GetAsync("pokemon/3");
+            Pokemon pokemon = await response.Content.ReadAsAsync<Pokemon>();
+
+            return pokemon;
+        }
+    }
+}
