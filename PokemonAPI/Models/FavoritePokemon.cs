90b3e1f026edfada4cda5bdda9675c5809942273
JordanHallam <jghals12@gmail.com>
2020-09-03 10:17:23 -0400

    Wonky git stuff

---
+++ PokemonAPI/Models/FavoritePokemon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PokemonAPI.Models
+{
+    public partial class FavoritePokemon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string NickName { get; set; }
+        public int? PokemonId { get; set; }
+        public string UserId { get; set; }
+
+        public virtual AspNetUsers User { get; set; }
+    }
+}
